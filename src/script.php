8aba207c4541b15c35afdcbd99e6a4fcb3be239c
Gytis Karčiauskas <gytis@karciauskas.lt>
2017-08-25 22:23:59 +0300

    Integrate CommissionCalculation service with Discounts.

--- src/script.php
+++ src/script.php
@@ -4,8 +4,10 @@ require_once 'config.php';
 require_once '../vendor/autoload.php';
 
 use Persistence\MemoryPersistence;
+use Repository\DiscountRepository;
 use Repository\OperationRepository;
 use Repository\UserRepository;
+use Service\CommissionCalculationService;
 use Service\Currency\ExchangeService;
 use Service\Currency\RatesService;
 
@@ -13,17 +15,35 @@ $memoryPersistence = new MemoryPersistence();
 
 $userRepository = new UserRepository($memoryPersistence);
 $operationRepository = new OperationRepository($memoryPersistence);
+$discountRepository = new DiscountRepository($memoryPersistence);
 
 $ratesService = new RatesService();
 $exchangeService = new ExchangeService($ratesService, DEFAULT_CURRENCY);
 
 $csvRows = array_map('str_getcsv', file($argv[1]));
 
-foreach ($csvRows as $csvRow) {
+foreach ($csvRows as $index => $csvRow) {
     $user = $userRepository->findOrCreate((int)$csvRow[1], $csvRow[2]);
-    $operation = $operationRepository->create($csvRow[0], $csvRow[3], $csvRow[4], $csvRow[5], $user);
+    $operation = $operationRepository->create(
+        $index + 1,
+        $csvRow[0],
+        $csvRow[3],
+        $csvRow[4],
+        $csvRow[5],
+        $user,
+        $discountRepository
+    );
 }
+
+$commissionCalculatorService = new CommissionCalculationService(
+    $operationRepository,
+    $discountRepository,
+    $exchangeService
+);
 
 $operations = $operationRepository->getAll();
 
-$exchangeService->calculateRate(10, 'USD');
+foreach ($operations as $operation) {
+    $commissionCalculatorService->calculate($operation);
+    echo $commissionCalculatorService->getFormattedCommission() . PHP_EOL;
+}
