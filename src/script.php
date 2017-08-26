42d4ca8cb71fcf4991dfab96a94e89bb34937784
Gytis Karčiauskas <gytis@karciauskas.lt>
2017-08-26 15:46:09 +0300

    Use absolute paths.

--- src/script.php
+++ src/script.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once 'config.php';
-require_once '../vendor/autoload.php';
+require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Persistence\MemoryPersistence;
 use Repository\DiscountRepository;
